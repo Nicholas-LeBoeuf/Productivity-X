57a5e75c86f8af741019841f4f2bf8b4ac59a5d9
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2021-02-26 10:40:25 -0500

    Made new views, CreateAccount and Main. Set up MainWindow controller. Login Page ready for backend work... Next up is Create Account

---
+++ Productivity-X/Controllers/MainWindowController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Productivity_X.Controllers
+{
+    public class MainWindowController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+    }
+}
