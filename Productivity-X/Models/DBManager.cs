789304a5d881e525fbd0eba45f6fcb1cbb114622
Jason McKearney <jason.mckearney@snhu.edu>
2021-02-26 14:37:20 -0500

    added DBManager.cs and User.cs files

---
+++ Productivity-X/Models/DBManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Productivity_X.Models
+{
+	public class DBManager
+	{
+		public string ConnectionString { get; set; }
+		public DBManager(string connectionString)
+		{
+			this.ConnectionString = connectionString;
+		}
+
+		private MySqlConnection GetConnection()
+		{
+			return new MySqlConnection(ConnectionString);
+		}
+
+		public void SaveUser(User uc)
+		{
+			using (MySqlConnection conn = GetConnection())
+			{
+				conn.Open();
+				MySqlCommand Query = conn.CreateCommand();
+				Query.CommandText = "insert into Calendar_Schema.user_tbl (user_id, firstname, lastname, username, email, password, confirmpassword, verificationcode) VALUES (@userID,@firstname,@lastname, @username, @email, @password, @confirmpassword, @verificationcode)";
+				Query.Parameters.AddWithValue("@userID", uc.userID);
+				Query.Parameters.AddWithValue("@firstname", uc.firstname);
+				Query.Parameters.AddWithValue("@lastname", uc.lastname);
+				Query.Parameters.AddWithValue("@username", uc.username);
+				Query.Parameters.AddWithValue("@email", uc.email);
+				Query.Parameters.AddWithValue("@password", uc.password);
+				Query.Parameters.AddWithValue("@confirmpassword", uc.confirmpassword);
+				Query.Parameters.AddWithValue("@verificationcode", uc.verificationcode);
+
+				Query.ExecuteNonQuery();
+			}
+		}
+	}
+}
---
+++ Productivity-X/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Productivity_X.Models
+{
+	public class User
+	{
+		[Key]
+		public int userID { get; set; }
+
+		[Required(ErrorMessage = "Please Enter Firstname..")]
+		public string firstname { get => firstname; set => firstname = ""; }
+
+		[Required(ErrorMessage = "Please Enter Lastname..")]
+		public string lastname { get => lastname; set => firstname = ""; }
+
+		[Required(ErrorMessage = "Please Enter sername..")]
+		public string username { get => username; set => firstname = ""; }
+
+		[Required(ErrorMessage = "Please Enter your email..")]
+		public string email { get => email; set => firstname = ""; }
+
+		[Required(ErrorMessage = "Please Enter first name..")]
+		public string password { get => password; set => firstname = ""; }
+		public string confirmpassword { get => confirmpassword; set => firstname = ""; }
+		public string verificationcode { get => verificationcode; set => firstname = ""; }
+	}
+}
