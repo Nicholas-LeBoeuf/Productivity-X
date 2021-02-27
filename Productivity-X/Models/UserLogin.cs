68de034b713e1bf84f05d659ce31370e1cb7fae7
Jason McKearney <jason.mckearney@snhu.edu>
2021-02-27 14:32:36 -0500

    added UserLogin and UserCreatAccnt to Models folder

---
+++ Productivity-X/Models/UserLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Productivity_X.Models
+{
+	public class UserLogin
+	{
+		[Key]
+		public int userID { get; set; }
+
+		[StringLength(64, MinimumLength = 1)]
+		[Required(ErrorMessage = "Please Enter Username..")]
+		[Display(Name = "Username:")]
+		public string username { get; set; }
+
+		[StringLength(14, MinimumLength = 1)]
+		[Required(ErrorMessage = "Please Enter Password..")]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string password { get; set; }
+	}
+}
