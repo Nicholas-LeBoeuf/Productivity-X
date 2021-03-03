70acd6c53953280f74696b7cb9bc442a1cb4c7de
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2021-03-02 22:09:37 -0500

    Forgot password page 1 complete. Also added razor runtime compilation in startup.cs

---
+++ Productivity-X/Models/ForgotPw.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Productivity_X.Models
+{
+    public class ForgotPw
+    {
+        public string email { get; set; }
+
+    }
+}
