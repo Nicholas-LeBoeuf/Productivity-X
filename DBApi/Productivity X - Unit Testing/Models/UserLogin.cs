d7e8bae5e9b040ea3a735c643bdea4379b320407
Jason McKearney <60981210+JasonMcKearney@users.noreply.github.com>
2021-03-11 10:35:41 -0500

    Add files via upload

---
+++ DBApi/Productivity X - Unit Testing/Models/UserLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productivity_X___Unit_Testing.Models
+{
+	class UserLogin : DBObject
+	{
+		public string password;
+
+		public UserLogin(string sUsername,string sPassword)
+		{
+			DBObject.username = sUsername;
+			password = sPassword;
+		}
+	}
+}
