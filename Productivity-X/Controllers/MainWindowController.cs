c46dfcf0379c1f03f8e6dd63d1e61288e693fa60
Jason McKearney <jason.mckearney@snhu.edu>
2021-03-15 10:41:12 -0400

    Made changes to files for the backend to work properly

--- Productivity-X/Controllers/MainWindowController.cs
+++ Productivity-X/Controllers/MainWindowController.cs
@@ -22,8 +22,61 @@ namespace Productivity_X.Controllers
         }
         public IActionResult Weekly()
         {
+            // Will need to populate the calendar before opening it, query is in DBManager....
+
+            DBObject.categoriesForUserID = _manager.CategoryNamesForUserID(DBObject.nUserID);
+
             return View();
         }
+
+        public IActionResult CreateEvent(UserCreateEvent createEvent)
+        {
+            bool bRet = true;
+
+            if((createEvent.guest == true && createEvent.guestEmail == null && createEvent.guestUsername == null) || 
+                (createEvent.guest == false && createEvent.guestEmail != null && createEvent.guestUsername != null) || (createEvent.guest == false && createEvent.guestEmail != null && createEvent.guestUsername == null))
+			{           
+                bRet = false;
+			}
+
+            // True, save event to the database
+			if (bRet)
+			{
+                bRet = _manager.SaveEvent(createEvent);
+				if (bRet)
+				{
+                    ViewBag.message = "Event saved successfully!";
+				}
+				else
+				{
+                    ViewBag.message = "Event already exists!";
+                }
+            }
+			else
+			{
+                ViewBag.message = "Event was not saved, not all fields were filled in or were filled in incorrectly!";
+            }
+
+/*            bool bRet = false;
+            // Checks if all required fields are met
+            if (ModelState.IsValid)
+            {
+                bRet = _manager.SaveEvent(createEvent);
+
+                if (!bRet)
+                {
+                    // Go to Weekly page
+                    return View("Weekly");
+                }
+                else
+                {
+                    ViewBag.message = "Event Created Successfully!";
+                }
+            }
+*/
+            return View("Weekly");
+        }
+
         public IActionResult Today()
         {
             return View();
